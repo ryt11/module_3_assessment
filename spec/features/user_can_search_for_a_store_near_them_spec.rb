311678e66475c1b0bf516263f2024c94b9385980
Riley Thompson <rileyt_2000@hotmail.com>
2017-06-01 12:15:34 -0600

    remove extraneous text from spec

--- spec/features/user_can_search_for_a_store_near_them_spec.rb
+++ spec/features/user_can_search_for_a_store_near_them_spec.rb
@@ -5,15 +5,6 @@ RSpec.describe "Best Buy API Consumption" do
     click_on "Search"
     expect(current_path).to eq('/search')
     expect(page).to have_content("16 Total Stores")
-    
+
   end
 end
-
-As a user
-When I visit "/"
-And I fill in a search box with "80202" and click "search"
-Then my current path should be "/search" (ignoring params)
-And I should see stores within 25 miles of 80202
-And I should see a message that says "16 Total Stores"
-And I should see exactly 10 results
-And I should see the long name, city, distance, phone number and store type for each of the 10 results
